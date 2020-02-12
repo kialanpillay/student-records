18db43dca3e0a61fe41ec2ef68c819e1d8e93336
Kialan Pillay <kialanpillay@gmail.com>
2020-02-12 18:29:24 +0200

    Implemented Clear Method

---
+++ database.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <vector>
+#include "database.h"
+
+using namespace std;
+
+void clear(void)
+{
+    system("clear"); 
+}
